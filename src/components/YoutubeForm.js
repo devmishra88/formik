38039abfba64320a3fd8378131b158099481d49b
Devesh Mishra <devesh.mishra@intrcity.com>
2023-08-26 23:04:43 +0530

    basic form setup using formik

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "Devesh",
+      email: "",
+      channel: "",
+    },
+  })
+
+  console.log('Formik Values', formik.values)
+
+  return (
+    <div>
+      <form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            value={formik.values.channel}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;

2ad2105f95d7a52d4c8ed8bb05be49fa85a8838a
Devesh Mishra <devesh.mishra@intrcity.com>
2023-08-27 18:28:05 +0530

    initial value and function moved out

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,43 +1,49 @@
 import React from "react";
 import { useFormik } from "formik";
 
+const initialValues = {
+  name: "Devesh",
+  email: "",
+  channel: "",
+}
+
+const onSubmit = (values) => {
+  console.log("Form Data", values);
+}
+
+const validate = values=> {
+
+  //values.name values.email values.channel
+  //error.name error.email error.channel
+
+  let errors = {}
+
+  if(!values.name)
+  {
+    errors.name = 'Required'
+  }
+  if(!values.email)
+  {
+    errors.email = 'Required'
+  }
+  else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email))
+  {
+    errors.email = 'Invalid email format'
+  }
+
+  if(!values.channel)
+  {
+    errors.channel = 'Required'
+  }
+  
+  return errors
+}
+
 function YoutubeForm() {
   const formik = useFormik({
-    initialValues: {
-      name: "Devesh",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form Data", values);
-    },
-    validate: values=> {
-
-      //values.name values.email values.channel
-      //error.name error.email error.channel
-
-      let errors = {}
-
-      if(!values.name)
-      {
-        errors.name = 'Required'
-      }
-      if(!values.email)
-      {
-        errors.email = 'Required'
-      }
-      else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email))
-      {
-        errors.email = 'Invalid email format'
-      }
-
-      if(!values.channel)
-      {
-        errors.channel = 'Required'
-      }
-      
-      return errors
-    },
+    initialValues,
+    onSubmit,
+    validate
   });
 
   //console.log("Formik Values", formik.values);

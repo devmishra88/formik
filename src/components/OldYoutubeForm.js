0bb8a08a9caa3e9029c1be4bbb5cbf01c610e16a
Devesh Mishra <devesh.mishra@intrcity.com>
2023-08-28 22:54:35 +0530

    reduce repated code by using getFieldProps

---
+++ src/components/OldYoutubeForm.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { useFormik } from "formik"
+import * as Yup from 'yup'
+
+const initialValues = {
+  name: "Devesh",
+  email: "",
+  channel: "",
+}
+
+const onSubmit = (values) => {
+  console.log("Form Data", values);
+}
+
+const validationSchema = Yup.object({
+  name: Yup.string().required(`Name Required`),
+  email: Yup.string().email(`Invalid email format`).required(`Email Required`),
+  channel: Yup.string().required(`Channel Required`)
+})
+
+function OldYoutubeForm() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema
+    // validate,
+  });
+
+  console.log("Visited fields", formik.touched);
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            {...formik.getFieldProps(`name`)}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="error">{formik.errors.name}</div>
+          ) : null}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            {...formik.getFieldProps(`email`)}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            {...formik.getFieldProps(`channel`)}
+          />
+          {formik.touched.channel && formik.errors.channel ? (
+            <div className="error">{formik.errors.channel}</div>
+          ) : null}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default OldYoutubeForm;

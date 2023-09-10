8c01cbc080e4658c24e99819f1d4f953598cae70
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-10 17:10:19 +0530

    Input implemented with form control and container

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import FormikControl from "./FormikControl";
 
 function FormikContainer() {
-  const initialValues = {};
+  const initialValues = {
+    email: "",
+  };
 
-  const validationSchema = Yup.object({});
+  const validationSchema = Yup.object({
+    email: Yup.string().required(`Required`),
+  });
 
   const onSubmit = (values) => console.log(`Form Data`, values);
 
@@ -17,6 +22,12 @@ function FormikContainer() {
     >
       {(formik) => (
         <Form>
+          <FormikControl
+            control="input"
+            type="email"
+            label="Email"
+            name="email"
+          />
           <button type="submit">Submit</button>
         </Form>
       )}
--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Input from "./Input";
 
 function FormikControl(props) {
-  const { control } = props;
+  const { control, ...rest } = props;
 
   switch (control) {
     case "input":
+      return <Input {...rest} />
     case "textarea":
     case "select":
     case "radio":
     case "checkbox":
     case "data":
-    default:return null
+    default:
+      return null;
   }
 }
 

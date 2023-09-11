6979c41565e615baf8392097b8ec530aa7d13c4a
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-12 01:33:28 +0530

    login form with formik controller

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
-/*import YoutubeForm from './components/YoutubeForm'*/
-import FormikContainer from './components/FormikContainer'
+import LoginForm from './components/LoginForm'
+// import YoutubeForm from './components/YoutubeForm'
+// import FormikContainer from './components/FormikContainer'
 
 function App () {
   return (
     <div className='App'>
       {/* <YoutubeForm /> */}
-      <FormikContainer />
+      {/* <FormikContainer /> */}
+      <LoginForm />
     </div>
   )
 }
